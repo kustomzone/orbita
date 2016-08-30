34b1fc6f5839400eb52c6581f3ed153f2b423346
Alexandro <arvitaly@list.ru>
2016-08-31 02:01:37 +0700

    fix line

--- bin-start.js
+++ bin-start.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var electron = require('electron-prebuilt');
+var electron = require("electron-prebuilt");
 var proc = require('child_process');
 var args = process.argv.slice(2);
 args.unshift(__dirname + "/bin.js");
