2150e5d408ae1c09f6d5c84545a4113283833ad8
Alexandro <arvitaly@list.ru>
2016-10-25 21:26:26 +0700

    web security

--- default-window-opts.js
+++ default-window-opts.js
@@ -4,7 +4,8 @@ module.exports = {
     height: 768,
     webPreferences: {
         nodeIntegration: false,
-        preload: __dirname + '/preload.js'
+        preload: __dirname + '/preload.js',
+        webSecurity: false
     },
     userAgent: "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
     transports: {
